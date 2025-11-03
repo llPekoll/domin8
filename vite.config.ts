0be95dc77b384c7bc2c019e4384bc66045a29054
peko <yoyo.mepa@gmail.com>
2025-11-03 13:15:43 +0100

    feat: contdown

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tailwindcss(),
     nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
@@ -20,6 +20,6 @@ export default defineConfig({
     },
   },
   define: {
-    global: 'globalThis',
+    global: "globalThis",
   },
 });
